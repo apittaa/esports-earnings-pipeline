7e2244909d42fa5989bf47f7b72dd0667c019d64
Pitta <arthur_pitta@yahoo.com.br>
2024-02-15 18:51:51 -0300

    Adding dependencies to run tasks

--- terraform/bigquery.tf
+++ terraform/bigquery.tf
@@ -12,18 +12,27 @@ resource "google_bigquery_table" "esports_tournaments_bronze_table" {
   dataset_id = var.DATASET_BRONZE
   project    = var.PROJECT_ID
   table_id   = var.ESPORTS_TOURNAMENTS_BRONZE
+  depends_on = [
+    google_bigquery_dataset.dataset_bronze,
+  ]
 }
 
 resource "google_bigquery_table" "esports_games_genre_bronze_table" {
   dataset_id = var.DATASET_BRONZE
   project    = var.PROJECT_ID
   table_id   = var.ESPORTS_GAMES_GENRE_BRONZE
+  depends_on = [
+    google_bigquery_dataset.dataset_bronze,
+  ]
 }
 
 resource "google_bigquery_table" "esports_games_awarding_prize_money_bronze_table" {
   dataset_id = var.DATASET_BRONZE
   project    = var.PROJECT_ID
   table_id   = var.ESPORTS_GAMES_AWARDING_PRIZE_MONEY_BRONZE
+  depends_on = [
+    google_bigquery_dataset.dataset_bronze,
+  ]
 }
 
 # SILVER
@@ -37,18 +46,27 @@ resource "google_bigquery_table" "esports_tournaments_silver_table" {
   dataset_id = var.DATASET_SILVER
   project    = var.PROJECT_ID
   table_id   = var.ESPORTS_TOURNAMENTS_SILVER
+  depends_on = [
+    google_bigquery_dataset.dataset_silver,
+  ]
 }
 
 resource "google_bigquery_table" "esports_games_genre_silver_table" {
   dataset_id = var.DATASET_SILVER
   project    = var.PROJECT_ID
   table_id   = var.ESPORTS_GAMES_GENRE_SILVER
+  depends_on = [
+    google_bigquery_dataset.dataset_silver,
+  ]
 }
 
 resource "google_bigquery_table" "esports_games_awarding_prize_money_silver_table" {
   dataset_id = var.DATASET_SILVER
   project    = var.PROJECT_ID
   table_id   = var.ESPORTS_GAMES_AWARDING_PRIZE_MONEY_SILVER
+  depends_on = [
+    google_bigquery_dataset.dataset_silver,
+  ]
 }
 
 # GOLD
@@ -62,10 +80,16 @@ resource "google_bigquery_table" "esports_tournaments_with_genre_gold_table" {
   dataset_id = var.DATASET_GOLD
   project    = var.PROJECT_ID
   table_id   = var.ESPORTS_TOURNAMENTS_GOLD
+  depends_on = [
+    google_bigquery_dataset.dataset_gold,
+  ]
 }
 
 resource "google_bigquery_table" "esports_games_awarding_prize_money_with_genre_gold_table" {
   dataset_id = var.DATASET_GOLD
   project    = var.PROJECT_ID
   table_id   = var.ESPORTS_GAMES_AWARDING_PRIZE_MONEY_GOLD
+  depends_on = [
+    google_bigquery_dataset.dataset_gold,
+  ]
 }
