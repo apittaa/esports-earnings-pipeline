2557d610e50c932390a21808d3d8f2ddbbf42864
Pitta <arthur_pitta@yahoo.com.br>
2024-02-19 09:59:25 -0300

    Adding write_to_bq function

--- flows/silver/etl_gcs_silver_to_bq.py
+++ flows/silver/etl_gcs_silver_to_bq.py
@@ -11,7 +11,7 @@ import pyspark
 def extract_from_gcs(dataset_file: str) -> str:
     """Download data from GCS"""
     gcs_path = f"data/silver/{dataset_file}"
-    local_path = f""
+    local_path = ""
     gcs_block = GcsBucket.load("esports")
     gcs_block.get_directory(
         from_path=gcs_path,
@@ -19,3 +19,14 @@ def extract_from_gcs(dataset_file: str) -> str:
     )
 
     return gcs_path
+
+
+@task()
+def wite_to_bq(spark: pyspark, path: Path, df_name: str) -> None:
+    """write DataFrame to BigQuery"""
+    df = spark.read.format('delta').load(path)
+    df.write.format("bigquery") \
+            .option("writeMethod", "direct") \
+            .option('table', f'esports_data_all.{df_name}') \
+            .mode('overwrite') \
+            .save()
