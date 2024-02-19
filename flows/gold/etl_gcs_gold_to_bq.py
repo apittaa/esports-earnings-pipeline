7776301a2d84b80e4b64fc02a14617476e208403
Pitta <arthur_pitta@yahoo.com.br>
2024-02-19 19:01:18 -0300

    Adding schemas for tables with genre

---
+++ flows/gold/etl_gcs_gold_to_bq.py
@@ -0,0 +1,63 @@
+from delta import *
+from delta.tables import *
+
+from prefect import flow, task
+from prefect_gcp.cloud_storage import GcsBucket
+
+import pyspark
+
+from schemas.esports_schemas import ESPORTS_GAMES_AWARDING_PRIZE_MONEY_WITH_GENRE_SCHEMA, ESPORTS_TOURNAMENTS__WITH_GENRE_SCHEMA
+
+
+def extract_from_gcs(dataset_file: str) -> str:
+    """Download data from GCS"""
+    gcs_path = f"data/gold/{dataset_file}"
+    local_path = ""
+    gcs_block = GcsBucket.load("esports")
+    gcs_block.get_directory(
+        from_path=gcs_path,
+        local_path=local_path
+    )
+
+    return gcs_path
+
+
+@task()
+def wite_to_bq(spark: pyspark, path: str, df_name: str, schema: str) -> None:
+    """write DataFrame to BigQuery"""
+    df = spark.read.format('delta').load(path)
+    df.write.format("bigquery") \
+            .option("writeMethod", "direct") \
+            .option('table', f'esports_gold.{df_name}_with_genre') \
+            .option("schema", schema) \
+            .mode('overwrite') \
+            .save()
+            
+            
+@flow()
+def etl_gcs_gold_to_bq():
+    """The main ETL function"""  
+   
+    builder = pyspark.sql.SparkSession.builder.appName("test") \
+        .config("spark.executor.memory", "64g") \
+        .config("spark.sql.extensions", "io.delta.sql.DeltaSparkSessionExtension") \
+        .config("spark.sql.catalog.spark_catalog", "org.apache.spark.sql.delta.catalog.DeltaCatalog") \
+        .config("spark.jars", "spark-bigquery-with-dependencies_2.12-0.34.0.jar") \
+
+    spark = configure_spark_with_delta_pip(builder).getOrCreate()
+    spark.sparkContext.setLogLevel("ERROR") 
+    
+    dfs_name = {'esports_tournaments': ESPORTS_TOURNAMENTS__WITH_GENRE_SCHEMA, 
+                'esports_games_awarding_prize_money': ESPORTS_GAMES_AWARDING_PRIZE_MONEY_WITH_GENRE_SCHEMA
+                }
+
+    for df, schema in dfs_name.items():
+        path = extract_from_gcs(df)
+        wite_to_bq(spark, path, df, schema)
+
+    # End spark session
+    spark.stop()
+
+
+if __name__ == '__main__':
+    etl_gcs_gold_to_bq()
---
+++ flows/gold/schemas/esports_schemas.py
@@ -0,0 +1,73 @@
+from pyspark.sql.types import *
+
+ESPORTS_TOURNAMENTS_SCHEMA = StructType([
+    StructField('EndDate', DateType(), nullable=True),
+    StructField('GameId', IntegerType(), nullable=True),
+    StructField('Location', StringType(), nullable=True),
+    StructField('StartDate', DateType(), nullable=True),
+    StructField('Teamplay', IntegerType(), nullable=True),
+    StructField('TotalUSDPrize', FloatType(), nullable=True),
+    StructField('TournamentId', IntegerType(), nullable=True),
+    StructField('TournamentName', StringType(), nullable=True)
+])
+
+ESPORTS_GAMES_GENRE_SCHEMA = StructType([
+    StructField('GameName', StringType(), nullable=True),
+    StructField('Genre', StringType(), nullable=True),
+    StructField('GameId', IntegerType(), nullable=True)
+])
+
+ESPORTS_GAMES_AWARDING_PRIZE_MONEY_SCHEMA = StructType([
+    StructField('GameId', IntegerType(), nullable=True),
+    StructField('GameName', StringType(), nullable=True),
+    StructField('TotalPlayers', IntegerType(), nullable=True),
+    StructField('TotalTournaments', IntegerType(), nullable=True),
+    StructField('TotalUSDPrize', FloatType(), nullable=True)
+])
+
+ESPORTS_TOURNAMENTS__WITH_GENRE_SCHEMA = StructType([
+    StructField('GameId', IntegerType(), nullable=True),
+    StructField('TournamentId', IntegerType(), nullable=True),
+    StructField('TournamentName', StringType(), nullable=True),
+    StructField('StartDate', DateType(), nullable=True),
+    StructField('EndDate', DateType(), nullable=True),
+    StructField('Location', StringType(), nullable=True),
+    StructField('Teamplay', IntegerType(), nullable=True),
+    StructField('TotalUSDPrize', FloatType(), nullable=True),
+    StructField('GameName', StringType(), nullable=True),
+    StructField('Genre', StringType(), nullable=True)
+])
+
+ESPORTS_GAMES_AWARDING_PRIZE_MONEY_WITH_GENRE_SCHEMA = StructType([
+    StructField('GameId', IntegerType(), nullable=True),
+    StructField('GameName', StringType(), nullable=True),
+    StructField('TotalUSDPrize', FloatType(), nullable=True),
+    StructField('TotalTournaments', IntegerType(), nullable=True),
+    StructField('TotalPlayers', IntegerType(), nullable=True),
+    StructField('Genre', StringType(), nullable=True)
+])
+
+ESPORTS_TOURNAMENTS_TYPES = {
+    'EndDate': DateType(),
+    'GameId': IntegerType(),
+    'Location': StringType(),
+    'StartDate': DateType(),
+    'Teamplay': IntegerType(),
+    'TotalUSDPrize': FloatType(),
+    'TournamentId': IntegerType(),
+    'TournamentName': StringType()
+}
+
+ESPORTS_GAMES_GENRE_TYPES = {
+    'GameName': StringType(),
+    'Genre': StringType(),
+    'GameId': IntegerType()
+}
+
+ESPORTS_GAMES_AWARDING_PRIZE_MONEY_TYPES = {
+    'GameId': IntegerType(),
+    'GameName': StringType(),
+    'TotalPlayers': IntegerType(),
+    'TotalTournaments': IntegerType(),
+    'TotalUSDPrize': FloatType()
+}
