cc0a4a06c5b09edc89ffae74833ca2e7ad3b547e
Pitta <arthur_pitta@yahoo.com.br>
2024-02-22 18:55:51 -0300

    Adding deploy_ingest_gold function

---
+++ flows/gold/deploy_ingest_gold.py
@@ -0,0 +1,17 @@
+from prefect.deployments import Deployment
+from prefect.infrastructure.container import DockerContainer
+from ingest_gold import main_flow_gold
+
+
+docker_container_block = DockerContainer.load("esports-pipeline")
+
+esports_gold_deploy = Deployment.build_from_flow(
+    flow=main_flow_gold,
+    name="esports-gold",
+    infrastructure=docker_container_block
+)
+
+
+if __name__ == "__main__":
+    esports_gold_deploy.apply()
+    
