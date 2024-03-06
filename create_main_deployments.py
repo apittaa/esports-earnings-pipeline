837136bcd78518c32dac5c1e38db6ee587a939f1
Pitta <arthur_pitta@yahoo.com.br>
2024-03-06 15:10:01 -0300

    Adding logs

--- create_main_deployments.py
+++ create_main_deployments.py
@@ -1,5 +1,9 @@
 import subprocess
 
+print('LOGGING: Deploying bronze flow')
 subprocess.run("python flows/bronze/deploy_ingest_bronze.py", shell=True, check=False)
+print('LOGGING: Deploying silver flow')
 subprocess.run("python flows/silver/deploy_ingest_silver.py", shell=True, check=False)
+print('LOGGING: Deploying gold flow')
 subprocess.run("python flows/gold/deploy_ingest_gold.py", shell=True, check=False)
+print('LOGGING: ENDING DEPLOYMENTS')
--- run_main_deployments.py
+++ run_main_deployments.py
@@ -2,9 +2,13 @@ from prefect.deployments import run_deployment
 
 
 def main():
+    print('LOGGING: Running esports bronze flow')
     run_deployment(name="main-flow-bronze/esports-bronze")
+    print('LOGGING: Running esports silver flow')
     run_deployment(name="main-flow-silver/esports-silver")
+    print('LOGGING: Running esports gold flow')
     run_deployment(name="main-flow-gold/esports-gold")
+    print('LOGGING: ENDING FLOWS')
     
 
 if __name__ == "__main__":
